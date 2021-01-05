99000db7ea695bd7ae6175048e14da4268a4b2d4
xxc-xxc <55835158+xxc-xxc@users.noreply.github.com>
2021-01-05 17:45:39 +0800

    配置zuul路由规则

--- zuul/src/main/java/com/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/zuul/ZuulApplication.java
@@ -2,10 +2,12 @@ package com.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy // 开启网关功能
+@SpringBootApplication
 public class ZuulApplication {
 
     public static void main(String[] args) {
